17a95757691a63e372c35184dd9ef5f4c0d250d3
sasangi12 <sasanginawodya@gmail.com>
2026-01-29 22:53:12 +0530

    Initial spring boot backend setup

---
+++ src/main/java/lk/icbt/ovr/RoomReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.icbt.ovr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/icbt/ovr/RoomReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.icbt.ovr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
